fb5914937950f9c8d280743fbd7ccf5c893388ad
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2024-04-03 19:45:26 +0300

    done

--- ConsoleApp18/Program.cs
+++ ConsoleApp18/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace _22._03
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.CursorVisible = false;
+            Func<string, string, bool> containsWord = (text, word) => text.Contains(word);
+
+            string sampleText = "Це приклад рядка для тестування.";
+            string wordToFind = "приклад";
+
+            bool result = containsWord(sampleText, wordToFind);
+            Console.WriteLine($"Чи містить рядок слово '{wordToFind}': {result}");
+        }
+    }
+}
