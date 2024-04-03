15453435c54757a619c3be3206d457c610367ade
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2024-04-03 19:48:36 +0300

    done

--- ConsoleApp18/Program.cs
+++ ConsoleApp18/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._03
+{
+    class Program3
+    {
+        static void Main()
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.CursorVisible = false;
+            Func<int, int> cube = number => number * number * number;
+
+            Console.WriteLine("Куб числа 3: " + cube(3));
+            Console.WriteLine("Куб числа 4: " + cube(4));
+        }
+    }
+}
