10a1c380d6ca086a02cf07fcebe9be68cf470121
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2024-04-03 19:49:47 +0300

    done

--- ConsoleApp18/Program.cs
+++ ConsoleApp18/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._03
+{
+    class Program5
+    {
+        static void Main()
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.CursorVisible = false;
+            int[] array = { 1, 3, 5, 7, 9 };
+
+            Func<int[], int> findMax = (numbers) =>
+            {
+                int max = numbers[0];
+                for (int i = 1; i < numbers.Length; i++)
+                {
+                    if (numbers[i] > max)
+                    {
+                        max = numbers[i];
+                    }
+                }
+                return max;
+            };
+
+            int maxNumber = findMax(array);
+            Console.WriteLine("Максимальне значення: " + maxNumber);
+        }
+    }
+}
